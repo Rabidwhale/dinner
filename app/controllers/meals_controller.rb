47e4beb739ea2d41045857effa26e3c5337fb8d4
Michael Williams <michaelwilliamsdvc15@gmail.com>
2019-07-02 04:51:14 +0000

    recipe

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,13 @@
+class MealsController < ApplicationController
+  def index
+  end
+
+  def scallop
+  end
+
+  def ribeye
+  end
+
+  def dessert
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'meals#index'
+  resources :meals
+  get 'scallop', to: 'meals#scallop'
+  get 'ribeye', to: 'meals#ribeye'
+  get 'dessert', to: 'meals#dessert'
 end
